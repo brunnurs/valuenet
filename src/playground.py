cca2228a1cec64d29d5541fadd9c26e60380542a
Ursin <ursin.brunner@gmail.com>
2020-01-21 17:32:51 +0100

    First part of the transformer-encoder done. Now we have to encoder the columns/tables.

--- src/playground.py
+++ src/playground.py
@@ -1,10 +1,33 @@
-import os
+from transformers import BertConfig, BertModel, BertTokenizer
 
-from src.spider.evaluation.spider_evaluation import spider_evaluation, build_foreign_key_map_from_json
+from src.model.encoder.encoder import TransformerEncoder
+from src.model.encoder.input_features import encode_input
+from src.utils import setup_device, set_seed_everywhere
 
-kmaps = build_foreign_key_map_from_json("data/spider/tables.json")
+config_class, model_class, tokenizer_class = (BertConfig, BertModel, BertTokenizer)
+config = config_class.from_pretrained("bert-base-uncased")
+tokenizer = tokenizer_class.from_pretrained("bert-base-uncased")
 
-spider_evaluation(os.path.join("experiments/exp__20200115_191836/ground_truth.txt"),
-                  os.path.join("experiments/exp__20200115_191836/output.txt"),
-                  os.path.join("data/spider/original/database"),
-                  "match", kmaps)
+question = [['what'], ['are'], ['name'], ['of'], ['all'], ['column', 'state'], ['with'], ['table', 'college'], ['student'], ['playing'], ['in'], ['mid'], ['position'], ['but'], ['no'], ['goalie'], ['?']]
+question2 = [['what'], ['are'], ['name'], ['of'], ['all'], ['column', 'state'], ['with'], ['table', 'college'], ['student'], ['playing'], ['in'], ['mid'], ['position'], ['?']]
+tables = [['college'], ['player'], ['tryout']]
+columns = [['count', 'number', 'many'], ['college', 'name'], ['state'], ['enrollment'], ['player', 'id'], ['player', 'name'], ['yes', 'card'], ['training', 'hour'], ['player', 'position'], ['decision']]
+
+# tokenized = tokenizer.encode_plus(text=question,
+#                               text_pair=columns + tables,
+#                               add_special_tokens=True,
+#                               max_length=100,
+#                               truncation_strategy='do_not_truncate',
+#                               pad_to_max_length=True)
+
+# encoded = encode_input([question, question2], [columns, columns], [tables, tables], tokenizer, 551, 'cpu')
+#
+# # print(encoded)
+device, n_gpu = setup_device()
+set_seed_everywhere(42, n_gpu)
+#
+encoder = TransformerEncoder('bert-base-uncased', device, 512)
+encoder.to(device)
+
+last_layer = encoder([question, question2], [columns, columns], [tables, tables])
+print(last_layer)
