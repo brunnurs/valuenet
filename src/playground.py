07e372daa13eff6a718a14079db7a95710e00f50
Ursin <ursin.brunner@gmail.com>
2020-01-27 13:45:53 +0100

    Finalized transformer encoding by creating column/table representation with the help of an LSTM. Not yet tested.

--- src/playground.py
+++ src/playground.py
@@ -11,7 +11,9 @@ tokenizer = tokenizer_class.from_pretrained("bert-base-uncased")
 question = [['what'], ['are'], ['name'], ['of'], ['all'], ['column', 'state'], ['with'], ['table', 'college'], ['student'], ['playing'], ['in'], ['mid'], ['position'], ['but'], ['no'], ['goalie'], ['?']]
 question2 = [['what'], ['are'], ['name'], ['of'], ['all'], ['column', 'state'], ['with'], ['table', 'college'], ['student'], ['playing'], ['in'], ['mid'], ['position'], ['?']]
 tables = [['college'], ['player'], ['tryout']]
-columns = [['count', 'number', 'many'], ['college', 'name'], ['state'], ['enrollment'], ['player', 'id'], ['player', 'name'], ['yes', 'card'], ['training', 'hour'], ['player', 'position'], ['decision']]
+tables2 = [['college'], ['player']]
+columns = [['count', 'number', 'many'], ['college', 'name'], ['state'], ['enrollment'], ['player', 'id'], ['player', 'name'], ['yes', 'card'], ['training', 'hour']]
+columns2 = [['count', 'number', 'many'], ['college', 'name'], ['state'], ['enrollment'], ['player', 'id'], ['player', 'name'], ['yes', 'card'], ['training', 'hour'], ['player', 'position'], ['decision']]
 
 # tokenized = tokenizer.encode_plus(text=question,
 #                               text_pair=columns + tables,
@@ -26,8 +28,8 @@ columns = [['count', 'number', 'many'], ['college', 'name'], ['state'], ['enroll
 device, n_gpu = setup_device()
 set_seed_everywhere(42, n_gpu)
 #
-encoder = TransformerEncoder('bert-base-uncased', device, 512)
+encoder = TransformerEncoder('bert-base-uncased', device, 512, 300, 300)
 encoder.to(device)
 
-last_layer = encoder([question, question2], [columns, columns], [tables, tables])
+last_layer = encoder([question, question2], [columns, columns2], [tables, tables2])
 print(last_layer)
