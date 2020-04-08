13750b3a55e769bab3017c582caae2f85d0f3c70
Ursin <ursin.brunner@gmail.com>
2020-04-08 08:38:22 +0200

    Implement further heuristics to find all possible values in the question.

---
+++ src/named_entity_recognition/google_api/extract_values_by_heuristics.py
@@ -0,0 +1,36 @@
+import re
+
+ORDINALS_AND_SIMILAR = {
+    'once': 1,
+    'twice': 2,
+    'thrice': 3,
+    'first': 1,
+    'second': 2,
+    'third': 3,
+    'fourth': 4,
+    'fifth': 5,
+    'sixth': 6,
+    'seventh': 7,
+    'eighth': 8,
+    'ninth': 9,
+    'tenth': 9,
+}
+
+
+def find_values_in_quota(question):
+    """
+    We try find all values in quotas, as this are almost always values we need later.
+    We support a variaty of different quota ascii characters.
+    """
+    matches = re.findall(r"[\"'‘“](.+?)[\"'’”]", question)
+    return [m for m in matches] + [f'%{m}%' for m in matches] + [f'%{m}' for m in matches] + [f'{m}%' for m in matches]
+
+
+def find_ordinals(question_tokens):
+    values_from_ordinals = []
+
+    for token in question_tokens:
+        if token in ORDINALS_AND_SIMILAR:
+            values_from_ordinals.append(str(ORDINALS_AND_SIMILAR[token]))
+
+    return values_from_ordinals
