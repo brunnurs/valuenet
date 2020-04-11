4706f41570018b7ef24b10ab59ff3a9bff293d0b
Ursin <ursin.brunner@gmail.com>
2020-04-11 09:21:41 +0200

    Fix some bugs in the database value finder. It is the tolerance value though still needs to get ajusted.

--- src/named_entity_recognition/database_value_finder/test_database_value_finder.py
+++ src/named_entity_recognition/database_value_finder/test_database_value_finder.py
@@ -22,7 +22,7 @@ class TestDatabaseValueFinder(TestCase):
 
     def test_find_similar_values_in_database_adjective(self):
         # GIVEN
-        potential_values = ['routes', 'Canadian', 'airport']
+        potential_values = ['Canadian', 'airport', 'routes']
         db_name = 'flight_4'
         db_folder = 'data/spider/original/database'
         db_schemas = 'data/spider/tables.json'
@@ -34,7 +34,41 @@ class TestDatabaseValueFinder(TestCase):
 
         # THEN
         print(similar_values_db)
-        # self.assertGreaterEqual(similar_values_db.index(('Professor', 'Rank', 'Faculty')), 0, 'Could not find "Professor" in database.')
+        self.assertGreaterEqual(similar_values_db.index(('Canada', 'country', 'airports')), 0, 'Could not find "Professor" in database.')
+
+    def test_find_similar_values_in_database_italy_italian(self):
+        # TODO 'Italian' and 'Italy' is just too far away for the string similarity. What can we do?
+        # GIVEN
+        potential_values = ['Italian']
+        db_name = 'flight_4'
+        db_folder = 'data/spider/original/database'
+        db_schemas = 'data/spider/tables.json'
+
+        db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
+
+        # WHEN
+        similar_values_db = db_value_finder.find_similar_values_in_database(potential_values)
+
+        # THEN
+        print(similar_values_db)
+        self.assertGreaterEqual(similar_values_db.index(('Italy', 'country', 'airports')), 0, 'Could not find "Professor" in database.')
+
+    def test_find_similar_values_in_database___fixed_weird_column_name(self):
+        # GIVEN
+        potential_values = ['outcomes', 'project', 'details', 'patent', 'paper', 'project details']
+        db_name = 'tracking_grants_for_research'
+        db_folder = 'data/spider/original/database'
+        db_schemas = 'data/spider/tables.json'
+
+        db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
+
+        # WHEN
+        similar_values_db = db_value_finder.find_similar_values_in_database(potential_values)
+
+        # THEN
+        print(similar_values_db)
+        self.assertGreaterEqual(similar_values_db.index(('Paper', 'outcome_code', 'Research_Outcomes')), 0,
+                                'Could not find "Paper" in database.')
 
     def test__assemble_query(self):
         # GIVEN
