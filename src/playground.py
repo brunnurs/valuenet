4377757986d12c74e10252ef03b0591bf8e398aa
Ursin <ursin.brunner@gmail.com>
2020-04-12 14:47:39 +0200

    Add NER data which are needed as part of the pre-processing.

--- src/playground.py
+++ src/playground.py
@@ -1,4 +1,13 @@
-from spider.spider_utils import load_data_new
+import json
 
-dummy = load_data_new('data/spider/train.json', [], use_small=False)
-print(dummy)
+with open('data/spider/dev.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+ner_values_only = []
+for row in data:
+    ner_extracted_values = row['ner_extracted_values']
+    ner_extracted_values['question'] = row['question']
+    ner_values_only.append(ner_extracted_values)
+
+with open('data/spider/ner_dev.json', 'w', encoding='utf-8') as f:
+    json.dump(ner_values_only, f)
