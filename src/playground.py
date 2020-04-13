6d1884d01d5dadc10c23ca39d58724f0d05a387b
Ursin <ursin.brunner@gmail.com>
2020-04-13 16:43:43 +0200

    Add new pre-processed train.json again. SemQL is no referencing to the ner_extracted_values_processed-list.

--- src/playground.py
+++ src/playground.py
@@ -1,13 +1,29 @@
 import json
 
-with open('data/spider/dev.json', 'r', encoding='utf-8') as json_file:
+# with open('data/spider/preprocessed_with_values.json', 'r', encoding='utf-8') as json_file:
+#     data = json.load(json_file)
+#     for row in data:
+#         values = row['values']
+#         if values:
+#             candidates = row['ner_extracted_values_processed']
+#             print(f'Values: {values}          Candiates: {candidates}')
+
+from helpers.get_values_from_sql import format_groundtruth_value
+
+with open('data/spider/train.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
-ner_values_only = []
+count = 0
 for row in data:
-    ner_extracted_values = row['ner_extracted_values']
-    ner_extracted_values['question'] = row['question']
-    ner_values_only.append(ner_extracted_values)
+    if len(row['values']) >= 3:
+        print(row['values'])
+        print(row['ner_extracted_values_processed'])
+        print(row['rule_label'])
+        print(row['question'])
+        print(row['query'])
+        print()
+        print()
 
-with open('data/spider/ner_dev.json', 'w', encoding='utf-8') as f:
-    json.dump(ner_values_only, f, indent=2)
+        count += 1
+
+print(count)
