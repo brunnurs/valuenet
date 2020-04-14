e433c9c00a421835bb8625f14f0a044e66fb6e05
Ursin <ursin.brunner@gmail.com>
2020-04-14 19:24:41 +0200

    Improve DatabaseValueFinder() to only return the N most similar values. That way we avoid huge amount of false-positives as we had it before for e.g. "John F Kennedy Airport". Manually updated train.json.

--- src/named_entity_recognition/database_value_finder/test_database_value_finder.py
+++ src/named_entity_recognition/database_value_finder/test_database_value_finder.py
@@ -10,7 +10,7 @@ class TestDatabaseValueFinder(TestCase):
         potential_values = [(candidate, tolerance) for candidate in ['Kayaking', 'names', 'professors', 'Canoeing', '1']]
         db_name = 'activity_1'
         db_folder = 'data/spider/original/database'
-        db_schemas = 'data/spider/tables.json'
+        db_schemas = 'data/spider/original/tables.json'
 
         db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
 
@@ -27,7 +27,7 @@ class TestDatabaseValueFinder(TestCase):
         potential_values = [(candidate, tolerance) for candidate in ['Canadian', 'airport', 'routes']]
         db_name = 'flight_4'
         db_folder = 'data/spider/original/database'
-        db_schemas = 'data/spider/tables.json'
+        db_schemas = 'data/spider/original/tables.json'
 
         db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
 
@@ -40,13 +40,16 @@ class TestDatabaseValueFinder(TestCase):
 
     def test_find_similar_values_in_database_italy_italian(self):
         # GIVEN
-        tolerance = 0.5  # TODO: we have to set the tolerance value incredible low to get a match - this creates way to many false positives.
+        # TODO:  we have to set the tolerance value incredible low to get a match - this creates way to many false positives. This fact we
+        # TODO   then have to cover with a large max_results match... so we need to get better here!
+        tolerance = 0.5
+        max_results = 100
         potential_values = [(candidate, tolerance) for candidate in ['Italian']]
         db_name = 'flight_4'
         db_folder = 'data/spider/original/database'
-        db_schemas = 'data/spider/tables.json'
+        db_schemas = 'data/spider/original/tables.json'
 
-        db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
+        db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas, max_results=max_results)
 
         # WHEN
         similar_values_db = db_value_finder.find_similar_values_in_database(potential_values)
@@ -61,7 +64,7 @@ class TestDatabaseValueFinder(TestCase):
         potential_values = [(candidate, tolerance) for candidate in ['outcomes', 'project', 'details', 'patent', 'paper', 'project details']]
         db_name = 'tracking_grants_for_research'
         db_folder = 'data/spider/original/database'
-        db_schemas = 'data/spider/tables.json'
+        db_schemas = 'data/spider/original/tables.json'
 
         db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
 
@@ -72,6 +75,29 @@ class TestDatabaseValueFinder(TestCase):
         print(similar_values_db)
         self.assertGreaterEqual(similar_values_db.index(('Paper', 'outcome_code', 'Research_Outcomes')), 0,
                                 'Could not find "Paper" in database.')
+
+    def test_find_similar_values_in_database___find_way_too_many_results(self):
+        # GIVEN
+        potential_values = [('Kennedy International Airport', 0.8), ('John', 0.8),
+                            ('John F Kennedy International Airport', 0.75), ('John', 0.75), ('F', 0.75),
+                            ('Kennedy', 0.75), ('International', 0.75), ('Airport', 0.75), ('John F', 0.75),
+                            ('F Kennedy', 0.75), ('Kennedy International', 0.75), ('International Airport', 0.75),
+                            ('John F Kennedy', 0.75), ('F Kennedy International', 0.75),
+                            ('Kennedy International Airport', 0.75), ('John F Kennedy International', 0.75),
+                            ('F Kennedy International Airport', 0.75), ('routes', 0.75), ('number', 0.75)]
+        db_name = 'flight_4'
+        db_folder = 'data/spider/original/database'
+        db_schemas = 'data/spider/original/tables.json'
+
+        db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
+
+        # WHEN
+        similar_values_db = db_value_finder.find_similar_values_in_database(potential_values)
+
+        # THEN
+        print(similar_values_db)
+        self.assertGreaterEqual(similar_values_db.index(('John F Kennedy International Airport', 'name', 'airports')), 0, 'Could not find "Paper" in database.')
+
 
     def test__assemble_query(self):
         # GIVEN
@@ -88,7 +114,7 @@ class TestDatabaseValueFinder(TestCase):
         # GIVEN
         db_name = 'tracking_grants_for_research'
         db_folder = 'data/spider/original/database'
-        db_schemas = 'data/spider/tables.json'
+        db_schemas = 'data/spider/original/tables.json'
 
         db_value_finder = DatabaseValueFinder(db_folder, db_name, db_schemas)
 
