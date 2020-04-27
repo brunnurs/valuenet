de20ae6d4d6550ab0598c397d46112e68ee9884c
Ursin <ursin.brunner@gmail.com>
2020-04-27 19:38:55 +0200

    Add further tools which helps with the paper

--- src/tools/visualize_values_vs_NER_candidates.py
+++ src/tools/visualize_values_vs_NER_candidates.py
@@ -25,8 +25,16 @@ with open('data/spider/preprocessed_with_values_train.json', 'r', encoding='utf-
             if diff >= 0:
                 all_diff.append(diff)
 
-    plt.hist(all_diff, bins=range(0, 30))
+    plt.hist(all_diff, bins=range(0, 10))
     # plt.hist(all_diff, bins=range(1, 30))
     # plt.hist(all_diff, bins=range(10, 50))
     plt.show()
     print(f'Total difference: {sum(all_diff)} in {len(data)} queries.')
+
+    # the following lines are just used t print out the values in the right format for the paper.
+    n_diff = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0}
+    for diff in all_diff:
+        n_diff[diff] += 1
+
+    for key, value in n_diff.items():
+        print(f'({key},{value})')
